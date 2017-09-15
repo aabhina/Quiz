e2072a6a6eee85f2daa3580d314d844ca1a52a4b
Anshul Abhinav <anshul@Anshuls-MacBook-Pro.local>
2017-09-15 14:01:48 -0600

    Till page 253, chapter 8.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -24,7 +24,10 @@ class ViewController: UIViewController {
 
     //@IBOutlet var questionLabel: UILabel!
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
     @IBOutlet var answerLabel: UILabel!
     
