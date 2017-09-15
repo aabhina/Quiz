55615ef16959421fa33ebaefca5acaf4f507ee03
Anshul Abhinav <anshul@Anshuls-MacBook-Pro.local>
2017-09-15 13:26:19 -0600

    Till the section Animation Completion, page 252

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,10 +12,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nextQuestionLabel.alpha = 0
+        
     }
 
-    @IBOutlet var questionLabel: UILabel!
+    //@IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    
     @IBOutlet var answerLabel: UILabel!
     
     let questions: [String] = [
@@ -36,13 +46,31 @@ class ViewController: UIViewController {
             currentQuestionIndex = 0
         }
         let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        animateLabelTransitions()
     }
     
     @IBAction func showAnswer(_ sender: UIButton) {
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
+    }
+    
+    func animateLabelTransitions() {
+        
+        //Animate the question label
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: [],
+                       animations: {
+                        self.currentQuestionLabel.alpha = 0
+                        self.nextQuestionLabel.alpha = 1
+                        },
+                       completion: {_ in
+                        swap(&self.currentQuestionLabel,
+                             &self.nextQuestionLabel)
+        })
     }
 
 }
